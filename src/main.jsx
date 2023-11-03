d10640e45bdfb6e1267c848f963f7c09d7e6764e
Fernando Alvarez <103456426+FerAlvarezVenti@users.noreply.github.com>
2023-11-03 15:42:40 -0300

    remove postbuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./AppRouter";
-
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.createRoot(rootElement).render(<AppRouter />);
 
 if (rootElement.hasChildNodes()) {
   ReactDOM.hydrateRoot(rootElement, <AppRouter />);
