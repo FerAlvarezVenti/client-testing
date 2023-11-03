8f6b5b017c73177feeb7a2cf54e8a83fe31f1a7d
Fernando Alvarez <103456426+FerAlvarezVenti@users.noreply.github.com>
2023-11-03 16:06:50 -0300

    downgrade to react 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
 import { AppRouter } from "./AppRouter";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-
 if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrateRoot(rootElement, <AppRouter />);
+  hydrate(<AppRouter />, rootElement);
 } else {
-  ReactDOM.createRoot(rootElement).render(<AppRouter />);
+  render(<AppRouter />, rootElement);
 }
