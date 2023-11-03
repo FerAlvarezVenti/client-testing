a8f8e6d94d1ca36d34603cdb62cb0f7f2a89ccf5
Fernando Alvarez <103456426+FerAlvarezVenti@users.noreply.github.com>
2023-11-03 16:41:35 -0300

    change tsx to jsx

---
+++ src/Event.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+
+const apiService = axios.create({
+  baseURL: "https://pre.venti.com.ar",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+function Event() {
+  const [event, setEvent] = useState();
+  const [loading, setLoading] = useState(true);
+  const { urlName } = useParams();
+
+  const getEvent = async () => {
+    setLoading(true);
+
+    const { data } = await apiService.get(`/api/event/name/${urlName}`);
+    setEvent(data.event);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getEvent();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (!event) return <div>Event not found</div>;
+
+  return (
+    <div>
+      <Helmet>
+        <title>{event.name}</title>
+        <meta property="og:title" content={event.name} />
+        <meta property="og:description" content={event.shortDescription} />
+        <meta property="og:image" content={event.promoImg} />
+        <meta property="og:url" content={window.location.href} />
+      </Helmet>
+      <button onClick={getEvent}>Get Event</button>
+      <p>{JSON.stringify(event)}</p>
+    </div>
+  );
+}
+
+export default Event;
